76386830f9fbba8a76a01013e27f145a86f9d093
James Canlas <jamescanlasjr@yahoo.com>
2021-02-21 04:28:44 -0800

    added user options after the prompt manager and also added promtEngineer

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const generateHtml = require('./src/page-template')
+const generateHtml = require('./src/page-template');
+const { listenerAdded } = require('emittery');
+const { choices } = require('yargs');
 const team = [];
 
 
 function promptManager() {
-
-        
     inquirer
         .prompt([{
                 type: "text",
@@ -31,15 +31,74 @@ function promptManager() {
                 name: "officeNumber",
                 message: "What is the Manager's Office Number?"
             }])
-            
+
+        .then(function (data) {
+            const name = data.name;
+            const id = data.id;
+            const email = data.email;
+            const officeNumber = data.officeNumber;
+            const teamMember = new Manager(name, id, email, officeNumber);
+            team.push(teamMember);
+            userOptions();
+        });
+}
+
+function userOptions() {
+    inquirer.prompt([{
+        type: 'list' ,
+        name: 'userChoice',
+        message: "Would you like to add more members?",
+        choices: ["Yes, add an Engineer", "Yes, add an Intern", "No, my team is done"]
+    }])
+    .then(function (data) {
+        switch (data.userChoice) {
+            case "Yes, add an Engineer":
+                promptEngineer();
+                break;
+
+            case "Yes, add an Intern":
+                //promptIntern();
+                break;
+            case "No, my team is done":
+                //function for team
+                break;
+        }
+    });
+}
+
+function promptEngineer() {
+    inquirer
+        .prompt([{
+                type: "text",
+                name: "name",
+                message: "What is the Engineer's name?"
+            },
+            {
+                type: "number",
+                name: "id",
+                message: "What is the Engineer's ID Number?"
+            },
+            {
+                type: "text",
+                name: "email",
+                message: "What is the Engineer's email address?"
+            },
+            {
+                type: "text",
+                name: "github",
+                message: "What is the Engineer's github?"
+            }])
+
         .then(function (data) {
             const name = data.name
             const id = data.id
             const email = data.email
-            const officeNumber = data.officeNumber
-            const teamMember = new Manager(name, id, email, officeNumber);
+            const github = data.github
+            const teamMember = new Engineer(name, id, email, github);
             team.push(teamMember)
+            console.log(teamMember);
             console.log(team)
+            
         });
 }
 
