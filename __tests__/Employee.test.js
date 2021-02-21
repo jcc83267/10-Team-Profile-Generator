5cfb30a5071a045c08b6d212da225c67edb6b39b
James Canlas <jamescanlasjr@yahoo.com>
2021-02-20 19:24:43 -0800

    added employee class and functions, also tested them and passed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,39 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+test(' Create an Employee object ', () => {
+    const employee = new Employee('Dave', 123, 'something@gmail.com');
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('something@gmail.com')
+    
+});
+
+test(' gets name ', () => {
+    const employee = new Employee('Dave', 123, 'something@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+    
+});
+
+test(' get Id ', () => {
+    const employee = new Employee('Dave', 123, 'something@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+    
+});
+
+test(' get email ', () => {
+    const employee = new Employee('Dave', 123, 'something@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+    
+});
+
+test(' get role ', () => {
+    const employee = new Employee('Dave', 123, 'something@gmail.com');
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+});
+
