59d945508e2b71d4a7b9fcca2ccaba81cdd5767d
James Canlas <jamescanlasjr@yahoo.com>
2021-02-21 07:09:58 -0800

    added hmtl and wrote to dist

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const generateHtml = require('./src/page-template');
-const { listenerAdded } = require('emittery');
-const { choices } = require('yargs');
+const fs = require('fs');
+const { cpuUsage } = require('process');
+
 const team = [];
 
 
 function promptManager() {
     inquirer
         .prompt([{
-                type: "text",
-                name: "name",
-                message: "What is the Manager's name?"
-            },
-            {
-                type: "number",
-                name: "id",
-                message: "What is the Manager's ID Number?"
-            },
-            {
-                type: "text",
-                name: "email",
-                message: "What is the Manager's email address?"
-            },
-            {
-                type: "text",
-                name: "officeNumber",
-                message: "What is the Manager's Office Number?"
-            }])
+            type: "text",
+            name: "name",
+            message: "What is the Manager's name?"
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "What is the Manager's ID Number?"
+        },
+        {
+            type: "text",
+            name: "email",
+            message: "What is the Manager's email address?"
+        },
+        {
+            type: "number",
+            name: "officeNumber",
+            message: "What is the Manager's Office Number?"
+        }])
 
         .then(function (data) {
             const name = data.name;
@@ -45,49 +45,50 @@ function promptManager() {
 
 function userOptions() {
     inquirer.prompt([{
-        type: 'list' ,
+        type: 'list',
         name: 'userChoice',
         message: "Would you like to add more members?",
         choices: ["Yes, add an Engineer", "Yes, add an Intern", "No, my team is done"]
     }])
-    .then(function (data) {
-        switch (data.userChoice) {
-            case "Yes, add an Engineer":
-                promptEngineer();
-                break;
+        .then(function (data) {
+            switch (data.userChoice) {
+                case "Yes, add an Engineer":
+                    promptEngineer();
+                    break;
 
-            case "Yes, add an Intern":
-                promptIntern();
-                break;
-            case "No, my team is done":
-                //function for team
-                break;
-        }
-    });
+                case "Yes, add an Intern":
+                    promptIntern();
+                    break;
+                case "No, my team is done":
+                    //teamHtml()
+                    compileTeam()
+                    break;
+            }
+        });
 }
 
 function promptEngineer() {
     inquirer
         .prompt([{
-                type: "text",
-                name: "name",
-                message: "What is the Engineer's name?"
-            },
-            {
-                type: "number",
-                name: "id",
-                message: "What is the Engineer's ID Number?"
-            },
-            {
-                type: "text",
-                name: "email",
-                message: "What is the Engineer's email address?"
-            },
-            {
-                type: "text",
-                name: "github",
-                message: "What is the Engineer's github?"
-            }])
+            type: "text",
+            name: "name",
+            message: "What is the Engineer's name?"
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "What is the Engineer's ID Number?"
+        },
+        {
+            type: "text",
+            name: "email",
+            message: "What is the Engineer's email address?"
+        },
+        {
+            type: "text",
+            name: "github",
+            message: "What is the Engineer's github?"
+        }])
 
         .then(function (data) {
             const name = data.name
@@ -96,16 +97,13 @@ function promptEngineer() {
             const github = data.github
             const teamMember = new Engineer(name, id, email, github);
             team.push(teamMember)
-            console.log(teamMember);
-            console.log(team)
             userOptions()
-            
         });
 }
 
 function promptIntern() {
     inquirer
-    .prompt([{
+        .prompt([{
             type: "text",
             name: "name",
             message: "What is the Intern's name?"
@@ -122,22 +120,112 @@ function promptIntern() {
         },
         {
             type: "text",
-            name: "github",
+            name: "school",
             message: "What is the Intern's current school?"
         }])
 
-    .then(function (data) {
-        const name = data.name
-        const id = data.id
-        const email = data.email
-        const school = data.school
-        const teamMember = new Intern(name, id, email, school);
-        team.push(teamMember)
-        console.log(teamMember);
-        console.log(team)
-        //userOptions()
+        .then(function (data) {
+            const name = data.name
+            const id = data.id
+            const email = data.email
+            const school = data.school
+            const teamMember = new Intern(name, id, email, school);
+            team.push(teamMember)
+            userOptions()
+        });
+}
+
+function compileTeam() {
+    const htmlArray = []
+    const htmlBeginning = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1 w-100 text-center fs-1 !important" style="font-size: 36px">My Team</span>
+    </nav>
+    <br/>
+    <div class="row justify-content-around" style="grid-row-gap: 20px">
+    `
+    htmlArray.push(htmlBeginning);
+
+    for (let i = 0; i < team.length; i++) {
+        let object = `
+        <div class="col-4 d-flex justify-content-center">
+            <div class="card" style="width: 40rem;">
+                <div class="card-header bg-primary text-white fs-3 fw-bold">
+                    <h4 class="bg-primary text-white fs-3 fw-bold text-center">
+                        ${team[i].name}
+                    </h4>
+                    <h6>
+                        ${team[i].getRole()} <span> ` 
+                        
+        if(team[i].getRole() === "Manager") {
+            object += `
+            <i class="fas fa-crown"></i> </span>
+            `
+        } else if(team[i].getRole() === "Engineer") {
+            object += `
+            <i class="fas fa-tools"></i> </span>
+            `
+        } else {
+            object += `
+            <i class="fas fa-pencil-alt"></i> </span>
+            `
+        }
+                    
+        object += `                
+                    </h6>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID Number: ${team[i].id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
         
-    });
+        `
+        if (team[i].officeNumber) {
+            object += `
+                        <li class="list-group-item">Office Number: ${team[i].officeNumber}</li>
+            `
+        }
+        if (team[i].github) {
+            object += `
+                        <li class="list-group-item">GitHub: <a href="https://github.com/${team[i].github}">${team[i].github}</a></li>
+            `
+        }
+        if (team[i].school) {
+            object += `
+                        <li class="list-group-item">Current School: ${team[i].school}</li>
+            `
+        }
+        object += `
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `
+        htmlArray.push(object)
+    }
+
+    const htmlEnd = `
+    </div>
+</body>
+</html>
+`
+    htmlArray.push(htmlEnd);
+
+    fs.writeFile(`./dist/index.html`, htmlArray.join(""), function (err) {
+
+    })
 }
 
 promptManager();
